5ca8f279b097b3ff396f24d229ea829e4ef78eab
John Gulbronson <john@benzinga.com>
2025-05-09 05:09:25 -0400

    stylesheet manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { DefaultSeo, NextSeo } from "next-seo";
+import { StyleSheetManager } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { metadata } = pageProps;
   return (
     <>
-      <DefaultSeo
-        titleTemplate="%s | Collaborative Platform"
-        defaultTitle="Collaborative Platform"
-        description="A platform for collaborative projects and events"
-      />
-      {metadata && <NextSeo {...metadata} />}
-      <Component {...pageProps} />
+      <StyleSheetManager shouldForwardProp={(prop) => !prop.startsWith('$')}>
+        <DefaultSeo
+          titleTemplate="%s | Collaborative Platform"
+          defaultTitle="Collaborative Platform"
+          description="A platform for collaborative projects and events"
+        />
+        {metadata && <NextSeo {...metadata} />}
+        <Component {...pageProps} />
+      </StyleSheetManager>
     </>
   );
 }
