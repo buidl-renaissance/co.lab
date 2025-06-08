ea606f005ca8b87e03a22a32b4f7e4e161a04e7c
John Gulbronson <john@benzinga.com>
2025-06-07 23:39:39 -0400

    more growth

--- src/pages/api/collaborations.ts
+++ src/pages/api/collaborations.ts
@@ -12,6 +12,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === 'GET') {
     try {
       const collaborations = await getAllCollaborations();
